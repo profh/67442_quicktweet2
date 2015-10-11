113fcdffab67d06cb2d176819958037ce5be31f1
profh <profh@cmu.edu>
2015-10-11 17:45:14 -0400

    Basic app running

--- QuickTweet/ViewController.swift
+++ QuickTweet/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var accountName: UITextField!
+  @IBOutlet weak var twitterWebView: UIWebView!
+  
+  var account: String?
+  var url = NSURL (string:"http://www.twitter.com/profh")
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,30 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
+  @IBAction func sendTweet(sender: UIButton) {
+    if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+      let tweetVC = SLComposeViewController (forServiceType: SLServiceTypeTwitter)
+      tweetVC.setInitialText(
+        "Testing a twitter app for 67-442")
+      self.presentViewController(tweetVC, animated: true, completion: nil)
+    } else {
+      println ("Can't send tweet")
+    }
+  }
+  
+  @IBAction func setAccount() {
+    if accountName != nil {
+      account = accountName.text
+      println(account!)
+      url = NSURL (string:"http://www.twitter.com/\(account!)")
+    }
+  }
+  
+  @IBAction func showTweets(sender: UIButton) {
+    let urlRequest = NSURLRequest (URL: url!)
+    twitterWebView.loadRequest(urlRequest)
+  }
 
 }
 
